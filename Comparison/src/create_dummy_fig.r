c153f21ee23fc036398c225d71cdc5aaadac7c70
emsonder <emanuel.sonder@hest.ethz.ch>
2026-06-07 18:42:53 +0200

    removed unused files

--- Comparison/src/create_dummy_fig.r
+++
@@ -1,7 +0,0 @@
-library(ggplot2)
-library(data.table)
-
-dt <- data.table(x=runif(10), y=runif(10))
-ggplot(dt, aes(x=x, y=y))+geom_point()+theme_bw()
-dir.create("plots/dummy_fig.png")
-ggsave("plots/dummy_fig.png")
